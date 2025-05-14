545d2dd4bd4b8cc438e6e5c2050f0787f6a8e379
Pedro Carriço <pedro.carrico@gmail.com>
2025-05-14 12:42:06 +0100

    Remove unneeded code and comments

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,22 +1,17 @@
 package version
 
 var (
-	// Version is the current version of the application
-	Version = "dev"
-	// BuildTime is the time when the application was built
+	Version   = "dev"
 	BuildTime = "unknown"
-	// GitCommit is the git commit hash
 	GitCommit = "unknown"
 )
 
-// Info contains version information
 type Info struct {
-	Version   string `json:"version"`
-	BuildTime string `json:"build_time"`
-	GitCommit string `json:"git_commit"`
+	Version   string
+	BuildTime string
+	GitCommit string
 }
 
-// Get returns the version information
 func Get() Info {
 	return Info{
 		Version:   Version,
